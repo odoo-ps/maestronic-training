c38c2fe907bbf6adff02a24d7adf9fefb4aef5e7
nni-odoo <nni@odoo.com>
2021-10-29 15:42:38 +0800

    DEV!

---
+++ car/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Car",
+
+    'summary': """
+        Car Model""",
+
+    'description': """
+        Representation of Car
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    'category': 'Uncategorized',
+    'version': '14.0.1.0.0',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'base',
+        'helpdesk'
+    ],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        "views/portal_helpdesk.xml",
+    ],
+}
